2e76d35bd3f653f1d9bee80cbece19edb4e55006
argen mukash <mukashevargen@gmail.com>
2023-02-18 17:36:53 +0600

    1.Инициализация репозитория GitHub

---
+++ src/test/java/kg/finalproject/natvkg/NatvkgApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.finalproject.natvkg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NatvkgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
